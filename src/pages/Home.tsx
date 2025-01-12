88ff5393193460254f904d8d337c72022b1ef279
Kailash Sankar <ksankar@conviva.com>
2025-01-12 15:11:18 +0530

    flashy landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,22 @@
-import { TypeEffect } from '@/layout/styled';
+import { TypeEffect } from '@/assets/styled';
 
 const Home = () => {
     return (
         <div className='text-center'>
-            <div className='text-xl inline-block'>
-                <TypeEffect>
-                    Dev Tools
+            <div className='inline-block'>
+            <TypeEffect>
+                <span className="key">D</span>
+                <span className="key">E</span>
+                <span className="key">V</span>
+                <span className="key">&nbsp;</span>
+                <span className="key">T</span>
+                <span className="key">O</span>
+                <span className="key">O</span>
+                <span className="key">L</span>
+                <span className="key">S</span>
                 </TypeEffect>
             </div>
+
             <div>
                 {`There are no API calls. Data stays in the browser, persists only on browser localStorage.`}
             </div>
